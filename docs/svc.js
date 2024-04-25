05bf6fa91a34a9624b6291fbf7b27def968a04ea
Jidapa Angsutti <jidapa.angsutti@students.snow.edu>
2024-04-25 12:13:13 -0600

    updated url called in client

--- docs/svc.js
+++ docs/svc.js
@@ -1,13 +1,15 @@
 
 
 export const GetAllUserAccounts = async () => {
-    const url = "http://localhost:5207/userAccounts";
+    // const url = "http://localhost:5207/userAccounts";
+    const url = "https://my-web-demo.azurewebsites.net/userAccounts";
     const response = await fetch(url);
     return await response.json();
 }
 
 export const AddUser = async (userName) => {
-    const url = "http://localhost:5207/userAccount";
+    // const url = "http://localhost:5207/userAccount";
+    const url = "https://my-web-demo.azurewebsites.net/userAccount";
     
     const newUser = {userName};
     await fetch(url, {
